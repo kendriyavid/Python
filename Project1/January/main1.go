2304d6f15a321680ca32ebf6c2fbc0901e686a5f
HARSHDEEP SINGH <harshdeepkv@gmail.com>
2025-01-20 21:41:13 +0530

    Update main1.go

--- Project1/January/main1.go
+++ Project1/January/main1.go
@@ -191,53 +191,51 @@
 // 	}
 // }
 
-package main
+// package main
 
-import (
-	"fmt"
-	"sync"
-)
+// import (
+// 	"fmt"
+// 	"sync"
+// )
 
-func hello(id int, channel chan int, wg *sync.WaitGroup) {
-	defer wg.Done() // Decrement the counter when the goroutine completes
-	fmt.Println("starting:", id)
-	channel <- id
-}
+// func hello(id int, channel chan int, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	fmt.Println("starting:", id)
+// 	channel <- id
+// }
 
-func main() {
-	// Create an array of 2 channels
-	var arr [2]chan int = [2]chan int{make(chan int), make(chan int)}
-	done := make(chan bool)
-	var wg sync.WaitGroup
+// func main() {
 
-	// Launch 10 goroutines
-	for i := 0; i < 10; i++ {
-		wg.Add(1) // Increment the counter for each goroutine
-		go hello(i, arr[i%2], &wg)
-	}
+// 	var arr [2]chan int = [2]chan int{make(chan int), make(chan int)}
+// 	done := make(chan bool)
+// 	var wg sync.WaitGroup
 
-	// Close the channels after all goroutines finish
-	go func() {
-		wg.Wait() // Wait for all goroutines to complete
-		close(arr[0])
-		close(arr[1])
-		done <- true // Signal the main loop to exit
-	}()
+// 	for i := 0; i < 10; i++ {
+// 		wg.Add(1)
+// 		go hello(i, arr[i%2], &wg)
+// 	}
 
-	// Loop to receive values from channels
-	for {
-		select {
-		case val, ok := <-arr[0]:
-			if ok {
-				fmt.Println("receiving from arr[0]:", val)
-			}
-		case val, ok := <-arr[1]:
-			if ok {
-				fmt.Println("receiving from arr[1]:", val)
-			}
-		case <-done:
-			fmt.Println("Exiting loop.")
-			return // Exit the loop
-		}
-	}
-}
+// 	go func() {
+// 		wg.Wait()
+// 		close(arr[0])
+// 		close(arr[1])
+// 		done <- true
+// 	}()
+
+// 	for {
+// 		select {
+// 		case val, ok := <-arr[0]:
+// 			if ok {
+// 				fmt.Println("receiving from arr[0]:", val)
+// 			}
+// 		case val, ok := <-arr[1]:
+// 			if ok {
+// 				fmt.Println("receiving from arr[1]:", val)
+// 			}
+// 		case <-done:
+// 			fmt.Println("Exiting loop.")
+// 			return
+// 		}
+// 	}
+// }
+
