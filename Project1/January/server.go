79540c54bd9ce58b840813c85f6d06f4cbb118f8
HARSHDEEP SINGH <harshdeepkv@gmail.com>
2025-01-22 15:45:55 +0530

    Update server.go

--- Project1/January/server.go
+++ Project1/January/server.go
@@ -1,37 +1,109 @@
+// package main
+
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"net"
+// )
+
+// func handle(conn net.Conn) {
+// 	newScanner := bufio.NewScanner(conn)
+// 	for newScanner.Scan() {
+// 		line := newScanner.Text()
+// 		fmt.Println(line)
+// 	}
+// 	defer conn.Close()
+// 	_, err := conn.Write([]byte("harshdeepsingh"))
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// }
+
+// func main() {
+
+// 	li, err := net.Listen("tcp", ":8080")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer li.Close()
+// 	for {
+// 		conn, err := li.Accept()
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		go handle(conn)
+// 	}
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var bs []byte = []byte("harshdeep")
+// 	fmt.Println(bs)
+// 	fmt.Print(string(fmt.Appendln(bs, "singh")))
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"io"
+// 	"strings"
+// )
+
+// func main() {
+// 	var name string
+// 	var age int
+// 	var added string = "harshdeepsingh 22"
+// 	var reader io.Reader = strings.NewReader(added)
+// 	fmt.Fscan(reader, &name, &age)
+// 	fmt.Printf("%[2]T \n", name, age)
+// 	fmt.Printf(name, age)
+
+// }
+
+// package main
+
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"io"
+// 	"strings"
+// )
+
+// func main() {
+
+// 	var name string = "harshdeepsinghab"
+// 	var r io.Reader = strings.NewReader(name)
+// 	bfr := *bufio.NewReader(r)
+// 	p := make([]byte, 10)
+// 	_, err := bfr.Read(p)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	fmt.Println(bfr.Buffered())
+// 	fmt.Println(string(p))
+// }
+
 package main
 
 import (
 	"bufio"
 	"fmt"
-	"net"
+	"strings"
 )
 
-func handle(conn net.Conn) {
-	newScanner := bufio.NewScanner(conn)
-	for newScanner.Scan() {
-		line := newScanner.Text()
-		fmt.Println(line)
-	}
-	defer conn.Close()
-	_, err := conn.Write([]byte("harshdeepsingh"))
-	if err != nil {
-		panic(err)
-	}
-
-}
-
 func main() {
-
-	li, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		panic(err)
+	str := "ffff\n de\n d\n dd\n"
+	r := strings.NewReader(str)
+	scr := bufio.NewScanner(r)
+	buf := make([]byte, 5)
+	scr.Buffer(buf, 1)
+	for scr.Scan() {
+		fmt.Println(scr.Text())
 	}
-	defer li.Close()
-	for {
-		conn, err := li.Accept()
-		if err != nil {
-			panic(err)
-		}
-		go handle(conn)
-	}
+	fmt.Println("ended")
 }
